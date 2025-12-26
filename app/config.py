8baf0691b0d5bba42e529185837a63d2df76a918
RandomUsername_Kr <theoriginaldongik@gmail.com>
2025-12-26 20:04:59 +0900

    Enhance config.py with debug print statements
    Added print statements for debugging database connection settings.

--- app/config.py
+++ app/config.py
@@ -1,24 +1,21 @@
-# Flask 설정 파일
-
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-# instance 폴더 경로
-INSTANCE_DIR = os.path.join(os.path.dirname(__file__), "..", "instance")
-
-# 폴더가 없으면 생성
-os.makedirs(INSTANCE_DIR, exist_ok=True)
-
 class Config:
     TURSO_DATABASE_URL = os.getenv("TURSO_DATABASE_URL")
     TURSO_AUTH_TOKEN = os.getenv("TURSO_AUTH_TOKEN")
 
-    if not TURSO_DATABASE_URL and TURSO_AUTH_TOKEN:
+    print(TURSO_AUTH_TOKEN)
+    print(TURSO_DATABASE_URL)
+
+    if TURSO_DATABASE_URL and TURSO_AUTH_TOKEN:
         # .env에 값이 있을 시에만 Turso를 사용하도록 설정
         SQLALCHEMY_DATABASE_URI = f"sqlite+{TURSO_DATABASE_URL}?secure=true"
         CONNECT_ARGS = {"auth_token": TURSO_AUTH_TOKEN}
+
+        print("클라우드 연결")
     else:
         # Turso 정보가 없거나 에러가 발생한다면 로컬 db를 사용하도록 설정
         print("\n[INFO] No Turso Setup: Use local db...\n")
